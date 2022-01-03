379f3748ba6d648ba6486801ce6407b3b0d969a3
Gahasa Purba <gahasapurba.tech@gmail.com>
2022-01-03 14:54:14 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// console.log(i);
+// i = 10;
+
+// function tes() {
+// 	for( var i = 0; i < 10; i++ ) {
+// 		console.log(i);
+// 	}
+// }
+// tes();
+// console.log(i);
+
+// for( let i = 0; i < 10; i++ ) {
+// 	console.log(i);
+// }
+// console.log(i);
+
+// const mhs = {
+// 	nama: "Gahasa Purba",
+// 	umur: 19
+// }
+
+// mhs.umur = 19;
+// mhs = {
+// 	jurusan : "Teknologi Rekayasa Perangkat Lunak"
+// }
+
+// console.log(mhs);
+
+const i = [1,2,3]
+
+// i = [1,2,3,4];
+i.push(4)
+
+console.log(i);
